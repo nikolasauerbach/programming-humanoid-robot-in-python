85f3144daa5f0f36b7b3559658bc6818861a78c1
nikolasauerbach <83692081+nikolasauerbach@users.noreply.github.com>
2021-11-04 14:46:33 +0100

    Task 1.0

--- introduction/get_sensor_data.py
+++ introduction/get_sensor_data.py
@@ -19,9 +19,9 @@ class MyAgent(SparkAgent):
     def think(self, perception):
         angle = 0
         temperature = 0
-        # YOUR CODE HERE
         # get angle and temperature to current data of joint HeadYaw
-
+        temperature = perception.joint_temperature['HeadYaw']
+        angle = perception.joint['HeadYaw']
         print('HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature))
         return super(MyAgent, self).think(perception)
 
